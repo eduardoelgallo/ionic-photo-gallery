01342c43c7b63a462d71bfbd0464c910a2ac32e0
Eduardo Alonso Escobar Lozano <adgallo79@gmail.com>
2023-11-21 21:32:15 -0600

    feat (gallery): now all photos are saved and load from fileSystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -10,9 +10,13 @@ import { UserPhoto } from './UserPhoto';
 export class PhotoService {
 
   public photos: UserPhoto[] = [];
+  private PHOTO_STORE: string = 'photos';
 
   constructor() { }
 
+  /**
+   * take a new photo
+   */
   public async addNewToGallery() {
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
@@ -20,9 +24,79 @@ export class PhotoService {
       quality: 100
     })
 
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
+    const savedImageFile = await this.savePicture(capturedPhoto)
+
+    this.photos.unshift(savedImageFile)
+
+    Preferences.set({
+      key: this.PHOTO_STORE,
+      value: JSON.stringify(this.photos)
     })
   }
+
+  /**
+   * save a photo
+   */
+  private async savePicture(photo: Photo) {
+
+    const base64Data = await this.readAsBase64(photo);
+
+    const fileName = Date.now() + '.jpeg';
+
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    })
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    }
+  }
+
+  /**
+   * Load photo saved
+   */
+  public async loadSaved() {
+    const { value } = await Preferences.get({key: this.PHOTO_STORE})
+
+    this.photos = (value ? JSON.parse(value) : []) as UserPhoto[];
+
+
+    // Display the photo by reading into base64 format
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+      });
+
+      // Web platform only: Load the photo as base64 data
+      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
+  }
+
+  /**
+   * made an http request for obtain a blob response
+   */
+  private async readAsBase64 (photo: Photo) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string
+  }
+
+  /**
+   * convert a blob file in base64
+   */
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result)
+    }
+
+    reader.readAsDataURL(blob)
+  })
 }
